c8ca0bf6c2ffc70217225f80e3d9b8b36051c4e1
Kristian Lindgren <kristian.lindgren@mogul.com>
2017-01-18 15:40:48 +0100

    added css for image spin

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,15 +7,17 @@ import pokeball from '../../img/logo/logo.png';
 // decides if game image should be faced up or down
 const Card = props => {
 	let image = "";
+	let classes = ["App-card"]
 
 	if (props.open === false) {
 		image = pokeball;
 	} else {
 		image = props.image;
+		classes.push("cardFlipped");
 	}
 
 	return (
-		<div className="App-card" onClick={props.onFlip}>
+		<div className={classes.join(' ')} onClick={props.onFlip}>
 			<img src={image} />
 		</div>
 	)
